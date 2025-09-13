46fe9bcce07525e61eae41661c4bb9160ad6368f
Atul Goyal <atulxgoyal@gmail.com>
2025-09-13 10:14:04 +0530

    second

--- testapp.py
+++ testapp.py
@@ -1,4 +1,7 @@
-from app import add 
+# test_app.py
+from app import add
+
 def test_add():
-    assert add(2,3) == 5
-    
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
